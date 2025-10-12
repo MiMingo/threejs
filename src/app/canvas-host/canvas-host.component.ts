b3b17af193889e28105efaf355b31102c7d76b49
mingo <wernertn@umich.edu>
2025-10-11 21:28:58 -0400

    responsive sizing

--- src/app/canvas-host/canvas-host.component.ts
+++ src/app/canvas-host/canvas-host.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import * as THREE from 'three';
 
 @Component({
@@ -8,22 +8,59 @@ import * as THREE from 'three';
   styleUrl: './canvas-host.component.scss',
 })
 export class CanvasHostComponent implements AfterViewInit {
-  @ViewChild('canvas') canvas!: HTMLCanvasElement;
+  @ViewChild('canvasRef') canvas!: ElementRef<HTMLCanvasElement>;
   renderer!: THREE.WebGLRenderer;
+  scene!: THREE.Scene;
   camera!: THREE.PerspectiveCamera;
   constructor() {}
 
   ngAfterViewInit() {
-    this.renderer = new THREE.WebGLRenderer({
-      antialias: true,
-      canvas: this.canvas,
-    });
+    // this.canvas.nativeElement.width = window.innerWidth;
+    // this.canvas.nativeElement.height = window.innerHeight;
+
+    this.scene = new THREE.Scene();
 
     this.camera = new THREE.PerspectiveCamera(
       75,
-      this.canvas.width / this.canvas.height,
-      0.1,
-      5,
+      this.canvas.nativeElement.width / this.canvas.nativeElement.height,
     );
+
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      canvas: this.canvas.nativeElement,
+    });
+
+    this.onResize();
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    const light = new THREE.DirectionalLight(0xffffff);
+    light.position.set(0, 0, 5);
+    this.scene.add(cube);
+    this.scene.add(light);
+    this.camera.position.z = 5;
+
+    this.renderer.setAnimationLoop(() => {
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      this.renderer.render(this.scene, this.camera);
+    });
+  }
+
+  animate(): void {
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  onResize(): void {
+    this.camera.aspect =
+      this.canvas.nativeElement.clientWidth /
+      this.canvas.nativeElement.clientHeight;
+    this.renderer.setSize(
+      this.canvas.nativeElement.clientWidth,
+      this.canvas.nativeElement.clientHeight,
+      false,
+    );
+    this.camera.updateProjectionMatrix();
   }
 }
